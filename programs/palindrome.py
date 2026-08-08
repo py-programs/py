2495e4514faa0944eef0038039e50e970c98fc9f
py-programs <python.programs35@gmail.com>
2026-08-08 14:09:37 +0545

    Create palindrome.py

---
+++ programs/palindrome.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number: "))
+
+original = n
+rev = 0
+
+while n > 0:
+    digit = n % 10
+    rev = rev * 10 + digit
+    n = n // 10
+
+if original == rev:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
