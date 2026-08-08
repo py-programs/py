3091c5e43661bba1455b1ed2050e3a250739b4da
py-programs <python.programs35@gmail.com>
2026-08-08 14:56:50 +0545

    Create sum_digits.py

---
+++ programs/sum_digits.py
@@ -0,0 +1,14 @@
+def sum_of_digits(n):
+    total = 0
+
+    while n > 0:
+        digit = n % 10
+        total = total + digit
+        n = n // 10
+
+    return total
+
+
+num = int(input("Enter a number: "))
+
+print("Sum of digits =", sum_of_digits(num))
