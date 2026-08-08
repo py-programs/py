dd722c3865879ccd3d20e67738e5647759fa80d3
py-programs <python.programs35@gmail.com>
2026-08-08 15:15:48 +0545

    Create count_digits.py

---
+++ programs/count_digits.py
@@ -0,0 +1,12 @@
+def count_digits(n):
+    count = 0
+
+    while n > 0:
+        count += 1
+        n //= 10
+
+    return count
+
+num = int(input("Enter a number: "))
+
+print("Number of digits =", count_digits(num))
